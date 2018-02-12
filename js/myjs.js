0a94a5681a6646511131f8b8d871c051118b03df
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-02-12 16:14:17 +0400

    add sections
    add sections

---
+++ js/myjs.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+// 	  if (scroll > 300) {
+// 	    $(".scroll-class").css("background" , "inherit");
+// 	  }
+
+// 	  else{
+// 		  $(".scroll-class").css("background" , "#ffffff");  	
+// 	  }
+//   })
+// })
