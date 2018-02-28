9075cdbf4fb801d1e79e02e6477d23ad4ebfb8f4
teemm <temuri.fxaladze02@geolab.edu.ge>
2018-02-28 19:05:18 +0400

    vaska edits
    vaska edits

--- js/myjs.js
+++ js/myjs.js
@@ -1,5 +1,72 @@
  // scroll
 
+// $("#about-usLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#about-us").offset().top
+//     }, 500);
+// });
+// $("#our-worksLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#our-works").offset().top
+//     }, 500);
+// });
+// $("#our-staffLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#our-staff").offset().top
+//     }, 500);
+// });
+// $("#servicesLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#services").offset().top
+//     }, 500);
+// });
+// $("#button").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#demo").offset().top
+//     }, 100);
+// });
+// console.log("ready in");
+
+// see more
+// function toggleText()
+// {
+//    document.getElementById("demo").classList.remove("collapse")
+// }
+
+// video Magnific Popup
+
+
+// $(document).on('ready', function() {
+      
+//       $('.video').magnificPopup({
+//         type: 'iframe',
+//         iframe: {
+//           patterns: {
+//             dailymotion: {
+//               index: 'dailymotion.com',
+//               id: function(url) {        
+//                   var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+//                   if (m !== null) {
+//                       if(m[4] !== undefined) {
+                        
+//                           return m[4];
+//                       }
+//                       return m[2];
+//                   }
+//                   return null;
+//               },
+//               src: 'https://www.dailymotion.com/embed/video/%id%'
+//             }
+//           }
+//         }
+//       });
+
+// slick slider
+
+
+$(function() {
+  //bla bla bla 
+
 $("#about-usLink").click(function() {
     $('html, body').animate({
         scrollTop: $("#about-us").offset().top
@@ -27,89 +94,44 @@ $("#button").click(function() {
 });
 
 
-// see more
-function toggleText()
-{
-   
-document.getElementById("demo").classList.remove("collapse")
-}
 
-// video Magnific Popup
-
-
-// slick slider
-
-$(document).on('ready', function() {
-      $(".vertical-center-4").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 4,
-        slidesToScroll: 2
-      });
-      $(".vertical-center-3").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-      $(".vertical-center-2").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 2,
-        slidesToScroll: 2
-      });
-      $(".vertical-center").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-      });
-      $(".vertical").slick({
-        dots: true,
-        vertical: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-      $(".regular").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive:[
-          {
-            breakpoint: 1024,
-            settings:{
-              slidesToShow: 2,
-              slidesToScroll: 1
+// video 
+$('.video').magnificPopup({
+  type: 'iframe',
+  
+  
+  iframe: {
+    patterns: {
+      dailymotion: {
+       
+        index: 'dailymotion.com',
+        
+        id: function(url) {        
+            var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+            if (m !== null) {
+                if(m[4] !== undefined) {
+                  
+                    return m[4];
+                }
+                return m[2];
             }
-          },
-          {
-            breakpoint: 740,
-            settings:{
-              slidesToShow:1,
-              slidesToScroll:1
-            }
-          }
-        ]
-      });
+            return null;
+        },
+        
+        src: 'https://www.dailymotion.com/embed/video/%id%'
+        
+      }
+    }
+  }
+  
+  
+});
 
-      $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 5,
-        slidesToScroll: 3
-      });
 
-      $(".variable").slick({
-        dots: true,
-        infinite: true,
-        variableWidth: true
-      });
-      $(".lazy").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true
-      });
-    });
+
+    $('.regular').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+});
