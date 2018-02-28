5edd938f04777e727bc13b181e8f2b061ee02f7f
teemm <temuri.fxaladze02@geolab.edu.ge>
2018-02-28 19:30:05 +0400

    edits
    edits

--- js/myjs.js
+++ js/myjs.js
@@ -1,62 +1,4 @@
- // scroll
 
-// $("#about-usLink").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#about-us").offset().top
-//     }, 500);
-// });
-// $("#our-worksLink").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#our-works").offset().top
-//     }, 500);
-// });
-// $("#our-staffLink").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#our-staff").offset().top
-//     }, 500);
-// });
-// $("#servicesLink").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#services").offset().top
-//     }, 500);
-// });
-// $("#button").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#demo").offset().top
-//     }, 100);
-// });
-// console.log("ready in");
-
-// see more
-
-
-// video Magnific Popup
-
-
-// $(document).on('ready', function() {
-      
-//       $('.video').magnificPopup({
-//         type: 'iframe',
-//         iframe: {
-//           patterns: {
-//             dailymotion: {
-//               index: 'dailymotion.com',
-//               id: function(url) {        
-//                   var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
-//                   if (m !== null) {
-//                       if(m[4] !== undefined) {
-                        
-//                           return m[4];
-//                       }
-//                       return m[2];
-//                   }
-//                   return null;
-//               },
-//               src: 'https://www.dailymotion.com/embed/video/%id%'
-//             }
-//           }
-//         }
-//       });
 
 // slick slider
 
@@ -120,7 +62,7 @@ $('.video').magnificPopup({
       }
     }
   }
-  
+
   
 });
 
