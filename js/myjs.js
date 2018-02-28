7a176c99809614d25a9a99503bb6b723e4051a28
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-03-01 01:27:43 +0400

    add slider text
    add slider text

--- js/myjs.js
+++ js/myjs.js
@@ -80,7 +80,23 @@ function toggleText()
 
     $('.regular').slick({
   infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToShow: 5,
+  slidesToScroll: 1,
+   responsive:[
+          {
+            breakpoint: 1024,
+            settings:{
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 740,
+            settings:{
+              slidesToShow:1,
+              slidesToScroll:1
+            }
+          }
+        ]
 });
 });
